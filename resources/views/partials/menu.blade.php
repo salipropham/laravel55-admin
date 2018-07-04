7d917edb42d4be4dbcd4247979dabed050233976
SaliproPham <salipropham@outlook.com>
2018-07-04 11:52:48 +0700

    mark active menu & add nojax attribute

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,20 +1,27 @@
 @if(Admin::user()->visible($item['roles']))
+
+    @php
+        $current = str_replace(trim(config('admin.route.prefix'),'/'),'',\Request::path());
+        $is_active = substr($current,0,strlen($item['uri'])) ===  $item['uri'];
+        $is_open = substr($current,0,strlen($item['uri'])) ===  $item['uri'];
+    @endphp
+
     @if(!isset($item['children']))
-        <li>
+        <li class="{{$is_active?'active':null}}">
             @if(url()->isValidUrl($item['uri']))
                 <a href="{{ $item['uri'] }}" target="_blank">
-            @else
-                 <a href="{{ admin_base_path($item['uri']) }}">
-            @endif
-                <i class="fa {{$item['icon']}}"></i>
-                <span>{{$item['title']}}</span>
-            </a>
+                    @else
+                        <a href="{{ admin_base_path($item['uri']) }}" {{ $item['pjax']?'':e('nojax') }}>
+                            @endif
+                            <i class="fa {{$item['icon']}}"></i>
+                            <span>{{__($item['title'])}}</span>
+                        </a>
         </li>
     @else
-        <li class="treeview">
+        <li class="treeview {{$is_open?'active':null}}">
             <a href="#">
                 <i class="fa {{$item['icon']}}"></i>
-                <span>{{$item['title']}}</span>
+                <span>{{  __($item['title'])  }}</span>
                 <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="treeview-menu">
